a6854617866734836340fe2326476440367b10b4
MarivaldoDev <pedromarivaldo10@gmail.com>
2025-06-09 20:30:32 -0300

    coletando os arquivos estáticos

--- project/asgi.py
+++ project/asgi.py
@@ -2,11 +2,12 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import app.routing 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 django_asgi_app = get_asgi_application()
+
+import app.routing 
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
