812cd04231006c11294f730df1056ae0492a0409
MarivaldoDev <pedromarivaldo10@gmail.com>
2025-06-09 21:43:21 -0300

    adicionando url redis

--- project/settings.py
+++ project/settings.py
@@ -62,7 +62,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [config("REDIS_URL", default="redis://localhost:6379")],
         },
     }
 }
