4db9ea03dbbade8cea1a58170ee809b127bb0acc
MarivaldoDev <pedromarivaldo10@gmail.com>
2025-06-12 09:32:30 -0300

    tentando servir os arquivos de media no servidor

--- project/settings.py
+++ project/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.railway.app','*']
 CSRF_TRUSTED_ORIGINS = [
